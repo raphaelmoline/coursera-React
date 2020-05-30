15e239c617e6960f28895c94a755467dfe8eacc7
Raphael Moline <raphael.moline_git@m4x.org>
2020-05-30 19:08:55 +0800

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom' ;
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -101,6 +102,10 @@ class CommentForm extends Component  {
 
 function RenderDish({dish}){
     return(
+      <FadeTransform in
+        transformProps = {{
+          exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
         <Card>
             <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
@@ -108,6 +113,7 @@ function RenderDish({dish}){
                 <CardText>{dish.description}</CardText>
             </CardBody>
         </Card>
+      </FadeTransform>
     );
 }
 
@@ -119,10 +125,14 @@ function RenderComments({comments, postComment, dishId}){
             return(
                 <div>
                   <ul key={comment.id} className="list-unstyled">
+                    <Stagger in>
+                      <Fade in>
                       <li>{comment.comment}</li>
                       <li>-- {comment.author},
                       {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
                       .format(new Date(Date.parse(comment.date)))}</li>
+                      </Fade>
+                    </Stagger>
                   </ul>
 
                 </div>
